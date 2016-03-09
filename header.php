fe67182281ff46eb1c868e0b198591edc44bf975
sovanda <MCR002@MCR002s-iMac.local>
2016-03-09 08:22:17 +0700

    Add search form

--- header.php
+++ header.php
@@ -41,12 +41,7 @@
             <?php if( !is_singular() ) echo '</h1>'; else echo '</h2>'; ?>
         </div><!-- .logo -->
         
-        <div id="directory-search" class="directory-search">
-            <form id="dir-search-form" method="get" class="dir-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                <input type="text" name="s" id="dir-search-text" placeholder="<?php echo __("Search keyword...", "sptheme");?>" class="dir-search-text">
-                <input type="submit" class="dir-search-submit" value="<?php echo __("Search", "sptheme");?>">
-            </form>
-        </div> <!-- #directory-search -->
+        <?php get_search_form(); ?>
 
         </div><!-- .container -->
     </header>
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<div id="directory-search" class="directory-search">
+    <form id="dir-search-form" method="get" class="dir-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <input type="text" name="s" id="dir-search-text" placeholder="<?php echo __("Search keyword...", "sptheme");?>" class="dir-search-text">
+        <input type="submit" class="dir-search-submit" value="<?php echo __("Search", "sptheme");?>">
+    </form>
+</div> <!-- #directory-search -->
